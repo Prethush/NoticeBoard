b307dffa9658af59c2ce186ead76a2c6c9317a39
prethush <ktmirash49@gmail.com>
2021-06-06 12:52:08 +0530

    added files

--- public/index.js
+++ public/index.js
@@ -1,19 +1,35 @@
 const form = document.querySelector(".form");
 const ul = document.querySelector(".sub-container");
-
+let cards = JSON.parse(localStorage.getItem("cards")) || [];
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    let obj = {};
     let title = form.elements.title;
     let category = form.elements.category;
-
-    let li = document.createElement("li");
-    let h3 = document.createElement("h3");
-    let h5 = document.createElement("h5");
-    li.style.height = "100px";
-    h3.innerText = title.value;
-    h5.innerText = category.value;
-    title.value = "";
-    category.value = "";
-    li.append(h5, h3);
-    ul.append(li);
+    obj.titleName = title.value;
+    obj.categoryName = category.value;
+    title.innerText = "";
+    category.innerText = "";
+    cards.push(obj);
+    localStorage.setItem("cards", JSON.stringify(cards));
+    createUI(cards);
+    console.log(localStorage);
 });
+
+function createUI(cards) {
+    ul.innerHTML = "";
+    cards.forEach((card) => {
+        let li = document.createElement("li");
+        li.style.height = "100px";
+        let h2 = document.createElement("h2");
+        let h5 = document.createElement("h5");
+        h2.innerText = card.titleName;
+        h5.innerText = card.categoryName;
+        li.append(h5, h2);
+        ul.append(li);
+    })
+   
+    
+}
+
+createUI(cards);
